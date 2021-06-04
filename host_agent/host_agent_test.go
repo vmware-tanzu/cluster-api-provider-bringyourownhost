42fe968e7bdf661266d0ac93e3890e1fbb98f82f
Anusha Hegde <anushah@vmware.com>
2021-06-04 06:55:31 +0000

    Add error checking to host agent
    Log errors when something goes wrong. Using klog for now, until we
    figure out how the controller runtime logger works.
    
    Signed-off-by: Jamie Monserrate <monserratej@vmware.com>

--- host_agent/host_agent_test.go
+++ host_agent/host_agent_test.go
@@ -8,11 +8,11 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 	infrastructurev1alpha4 "github.com/vmware-tanzu/cluster-api-provider-byoh/api/v1alpha4"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("HostAgent", func() {
-
 	It("should register the BYOHost with the management cluster", func() {
 		command := exec.Command(pathToHostAgentBinary, "--kubeconfig", kubeconfigFile.Name())
 		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -30,4 +30,31 @@ var _ = Describe("HostAgent", func() {
 		}).ShouldNot(BeNil())
 	})
 
+	It("should error out if the host already exists", func() {
+		ByoHost := &infrastructurev1alpha4.ByoHost{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ByoHost",
+				APIVersion: "infrastructure.cluster.x-k8s.io/v1alpha4",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      hostName,
+				Namespace: namespace,
+			},
+			Spec: infrastructurev1alpha4.ByoHostSpec{},
+		}
+		err = k8sClient.Create(context.Background(), ByoHost)
+
+		command := exec.Command(pathToHostAgentBinary, "--kubeconfig", kubeconfigFile.Name())
+		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(session).Should(gexec.Exit(255))
+	})
+
+	It("should retrun an error when invalid kubeconfig is passed in", func() {
+		command := exec.Command(pathToHostAgentBinary, "--kubeconfig", "non-existent-path")
+		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(255))
+	})
 })
