5071a0cbe88d446978a088a44e70ca670b6c61e6
Jamie Monserrate <monserratej@vmware.com>
2021-06-07 07:40:53 +0000

    Implement reconcile for host agent
    - The last test is flaky. Only passes when you run it alone
    - The Client is hard coded in the reconciler.
    
    Signed-off-by: Anusha Hegde <anushah@vmware.com>

---
+++ api/v1alpha4/condition_consts.go
@@ -0,0 +1,12 @@
+package v1alpha4
+
+import clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
+
+const (
+	// K8sComponentsInstalledCondition documents if the required Kubernetes
+	// components are currently installed on the node.
+	// This condition is managed by the host agent and it could be always true
+	// if the host is unmanaged; insteead, in case of managed host, it depends
+	// by the nodecurrently being hosting a BYOHmachine or not.
+	K8sComponentsInstalledCondition clusterv1.ConditionType = "K8sComponentsInstalled"
+)
