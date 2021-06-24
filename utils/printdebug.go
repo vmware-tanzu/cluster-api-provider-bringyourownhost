4ec1414301bfeae9aa42ad5e368ed7b1de3645e9
Hui Chen <huchen@huchen-a01.vmware.com>
2021-06-25 00:00:20 +0800

    delete the personal debug prefix

--- utils/printdebug.go
+++ utils/printdebug.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 )
 
-//temporary Unified prefix, for the convenience of searching log
-const DebugPrefix = "huchen:"
+//you can added this temporary unified prefix string when you debug your code, for the convenience of searching log
+const DebugPrefix = ""
 
 func PrintObj(prefix string, v interface{}) {
 	if v == nil {
