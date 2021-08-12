e840cc33ea4f5fe02d6537cf9b899e6de92f2981
Dharmjit Singh <sdharmjit@vmware.com>
2021-08-12 18:00:31 +0530

    Removed extra defer and unnecessary comments
    Signed-off-by: Anusha Hegde <anushah@vmware.com>

--- apis/infrastructure/v1alpha4/condition_consts.go
+++ apis/infrastructure/v1alpha4/condition_consts.go
@@ -4,16 +4,12 @@ import clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
 
 // Conditions and Reasons defined on BYOHost
 const (
-	// BootstrapReady                     clusterv1.ConditionType = "BootstrapReady"
 	// K8sNodeBootstrapSucceeded documents if the required Kubernetes
 	// components are currently installed on the node.
 	// This condition is managed by the host agent and it could be always true
 	// if the host is unmanaged; insteead, in case of managed host, it depends
-	// by the nodecurrently being hosting a BYOHmachine or not.
+	// by the node currently being hosting a BYOHmachine or not.
 	K8sNodeBootstrapSucceeded clusterv1.ConditionType = "K8sNodeBootstrapSucceeded"
-
-	// BootstrapReady is False
-	// TODO understand helper.patch semantics and accrodingly add appropriate reasons
 
 	// K8sNodeBootstrapSucceeded is False
 	WaitingForMachineRefReason           = "WaitingForMachineRefToBeAssigned"
@@ -29,6 +25,4 @@ const (
 	WaitingForClusterInfrastructureReason = "WaitingForClusterInfrastructure"
 	WaitingForBootstrapDataSecretReason   = "WaitingForBootstrapDataSecret"
 	BYOHostsUnavailableReason             = "BYOHostsUnavailable"
-
-	// think about conditions/reasons when byomachine is control plane node
 )
