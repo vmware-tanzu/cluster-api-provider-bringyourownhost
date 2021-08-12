47b2865febe09f55dc9fe5b9473df18dd783005a
Anusha Hegde <anushah@vmware.com>
2021-08-12 09:51:56 +0000

    Write integration test for host reconciler
    Created a new suite and wrote a test for checking the
    K8sNodeBootstrapSucceeded condition and  WaitingForMachineRefReason
    
    Signed-off-by: Dharmjit Singh <sdharmjit@vmware.com>

---
+++ agent/reconciler/reconciler_suite_test.go
@@ -0,0 +1,84 @@
+package reconciler
+
+import (
+	"go/build"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	infrastructurev1alpha4 "github.com/vmware-tanzu/cluster-api-provider-byoh/apis/infrastructure/v1alpha4"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+func TestReconciler(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Reconciler Suite")
+}
+
+var (
+	err        error
+	cfg        *rest.Config
+	k8sClient  client.Client
+	reconciler *HostReconciler
+	// clusterName           = "test-cluster"
+	testEnv *envtest.Environment
+	// defaultNamespace string = "default"
+)
+
+var _ = BeforeSuite(func() {
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "config", "crd", "bases"),
+			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", "cluster-api@v0.4.0", "config", "crd", "bases"),
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	cfg, err = testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	scheme := runtime.NewScheme()
+
+	err = infrastructurev1alpha4.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = corev1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = clusterv1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:             scheme,
+		MetricsBindAddress: ":6080",
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	reconciler = &HostReconciler{
+		Client: k8sClient,
+	}
+	err = reconciler.SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).NotTo(HaveOccurred())
+	}()
+})
+
+var _ = AfterSuite(func() {
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
