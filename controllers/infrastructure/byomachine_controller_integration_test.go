a8aec5ed9c9710a030094369e17f1e5622391f5f
Shailesh Pant <pshailesh@pshailesh-a01.vmware.com>
2021-08-17 23:50:20 +0530

    - Add License file  - Append license header in agent/ apis/ common/ controllers/ folders src files and main.go  - Add basic .golangci configuration file with License header checker  - Minor fmt issues fixed in agent folder -- Rebased main --   - Time 20th Aug

---
+++ controllers/infrastructure/byomachine_controller_integration_test.go
@@ -0,0 +1,118 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controllers
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	infrastructurev1alpha4 "github.com/vmware-tanzu/cluster-api-provider-byoh/apis/infrastructure/v1alpha4"
+	"github.com/vmware-tanzu/cluster-api-provider-byoh/common"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
+	"sigs.k8s.io/cluster-api/util/conditions"
+)
+
+var _ = Describe("Controllers/ByomachineController", func() {
+	Context("When a BYO Host is available", func() {
+		var (
+			ctx        context.Context
+			byoHost    *infrastructurev1alpha4.ByoHost
+			byoMachine *infrastructurev1alpha4.ByoMachine
+			machine    *clusterv1.Machine
+		)
+
+		BeforeEach(func() {
+			ctx = context.Background()
+			byoHost = common.NewByoHost(defaultByoHostName, defaultNamespace, nil)
+			Expect(k8sClient.Create(ctx, byoHost)).Should(Succeed())
+		})
+
+		It("claims the first available host", func() {
+			machine = common.NewMachine(defaultMachineName, defaultNamespace, defaultClusterName)
+			machine.Spec.Bootstrap = clusterv1.Bootstrap{
+				DataSecretName: &fakeBootstrapSecret,
+			}
+			Expect(k8sClient.Create(ctx, machine)).Should(Succeed())
+
+			byoMachine = common.NewByoMachine(defaultByoMachineName, defaultNamespace, defaultClusterName, machine)
+			Expect(k8sClient.Create(ctx, byoMachine)).Should(Succeed())
+			byoHostLookupKey := types.NamespacedName{Name: byoHost.Name, Namespace: byoHost.Namespace}
+
+			Eventually(func() bool {
+				createdByoHost := &infrastructurev1alpha4.ByoHost{}
+				err := k8sClient.Get(ctx, byoHostLookupKey, createdByoHost)
+				if err != nil {
+					return false
+				}
+				if createdByoHost.Status.MachineRef != nil {
+					if createdByoHost.Status.MachineRef.Namespace == defaultNamespace && createdByoHost.Status.MachineRef.Name == defaultByoMachineName {
+						return true
+					}
+				}
+				return false
+			}).Should(BeTrue())
+
+			byoMachineLookupkey := types.NamespacedName{Name: defaultByoMachineName, Namespace: defaultNamespace}
+			createdByoMachine := &infrastructurev1alpha4.ByoMachine{}
+
+			Eventually(func() string {
+				err := k8sClient.Get(ctx, byoMachineLookupkey, createdByoMachine)
+				if err != nil {
+					return ""
+				}
+				return createdByoMachine.Spec.ProviderID
+			}).Should(ContainSubstring(ProviderIDPrefix))
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, byoMachineLookupkey, createdByoMachine)
+				if err != nil {
+					return false
+				}
+				return createdByoMachine.Status.Ready
+			}).Should(BeTrue())
+
+			Eventually(func() corev1.ConditionStatus {
+
+				err := k8sClient.Get(ctx, byoMachineLookupkey, createdByoMachine)
+				if err != nil {
+					return corev1.ConditionFalse
+				}
+				readyCondition := conditions.Get(createdByoMachine, infrastructurev1alpha4.BYOHostReady)
+				if readyCondition != nil {
+					return readyCondition.Status
+				}
+				return corev1.ConditionFalse
+			}).Should(Equal(corev1.ConditionTrue))
+
+			node := corev1.Node{}
+			err := clientFake.Get(ctx, types.NamespacedName{Name: defaultNodeName, Namespace: defaultNamespace}, &node)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(node.Spec.ProviderID).To(ContainSubstring(ProviderIDPrefix))
+		})
+
+		AfterEach(func() {
+			Expect(k8sClient.Delete(ctx, byoMachine)).Should(Succeed())
+			Expect(k8sClient.Delete(ctx, byoHost)).Should(Succeed())
+			Expect(k8sClient.Delete(ctx, machine)).Should(Succeed())
+		})
+
+	})
+})
