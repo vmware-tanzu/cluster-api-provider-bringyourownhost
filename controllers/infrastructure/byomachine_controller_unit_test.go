5f779224a410ccb96efe3f6a16e388ca2d267513
Anusha Hegde <anushah@vmware.com>
2021-09-08 13:57:29 +0000

    Fix flaky controller unit tests
    Both unit and integration tests used the same reconciler object. The
    flakes arise when we try to read an object that was just created using
    k8sManager.GetClient() which read from cache. In this case, k8sClient
    has to be used. But in the case where Indexing is needed, we cannot use
    k8sClient, because that reads from the API Server and not the cache
    
    As a temporary fix, using separate reconciler objects for both unit and
    integration tests

--- controllers/infrastructure/byomachine_controller_unit_test.go
+++ controllers/infrastructure/byomachine_controller_unit_test.go
@@ -8,16 +8,21 @@ import (
 
 	infrastructurev1alpha4 "github.com/vmware-tanzu/cluster-api-provider-byoh/apis/infrastructure/v1alpha4"
 	"github.com/vmware-tanzu/cluster-api-provider-byoh/common"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
-	"sigs.k8s.io/cluster-api/controllers/remote"
-	"sigs.k8s.io/cluster-api/util/patch"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var _ = Describe("Controllers/ByomachineController/Unitests", func() {
+	var byoReconciler *ByoMachineReconciler
 
+	BeforeEach(func() {
+		byoReconciler = &ByoMachineReconciler{
+			Client: k8sClient,
+		}
+	})
 	Context("When byomachine is not found", func() {
 		var (
 			ctx context.Context
@@ -29,19 +34,20 @@ var _ = Describe("Controllers/ByomachineController/Unitests", func() {
 
 		BeforeEach(func() {
 			ctx = context.Background()
+
 		})
 
 		It("Should not attempt to reconcile when byomachine namespace does not exist", func() {
 			byoMachineLookupkey := types.NamespacedName{Name: defaultByoMachineName, Namespace: namespace}
 			request := reconcile.Request{NamespacedName: byoMachineLookupkey}
-			_, err := reconciler.Reconcile(ctx, request)
+			_, err := byoReconciler.Reconcile(ctx, request)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
 		It("Should not attempt to reconcile when byomachine name does not exist", func() {
 			byoMachineLookupkey := types.NamespacedName{Name: byoMachineName, Namespace: defaultNamespace}
 			request := reconcile.Request{NamespacedName: byoMachineLookupkey}
-			_, err := reconciler.Reconcile(ctx, request)
+			_, err := byoReconciler.Reconcile(ctx, request)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -76,8 +82,8 @@ var _ = Describe("Controllers/ByomachineController/Unitests", func() {
 			byoMachineLookupkey := types.NamespacedName{Name: byoMachine.Name, Namespace: defaultNamespace}
 			request := reconcile.Request{NamespacedName: byoMachineLookupkey}
 
-			_, err := reconciler.Reconcile(ctx, request)
-			Expect(err).To(MatchError("Cluster.cluster.x-k8s.io \"" + clusterName + "\" not found"))
+			_, err := byoReconciler.Reconcile(ctx, request)
+			Expect(err).To(MatchError("clusters.cluster.x-k8s.io \"" + clusterName + "\" not found"))
 		})
 
 		AfterEach(func() {
@@ -91,48 +97,32 @@ var _ = Describe("Controllers/ByomachineController/Unitests", func() {
 		// Reconcile assumes the node name equal to host name, or setNodeProviderID will be failed.
 		// We only have one node "host-unit-test" in testEnv, not "hostWhenNodeIsNotAvailable"
 		const (
-			// a DNS-1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character (e.g. 'example.com', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*')",
-			hostname    = "host-when-node-is-not-available"
-			machineName = "machine-when-node-is-not-available"
+			hostname = "host-when-node-is-not-available"
 		)
 
 		var (
 			ctx        context.Context
-			byoMachine *infrastructurev1alpha4.ByoMachine
 			byoHost    *infrastructurev1alpha4.ByoHost
-			machine    *clusterv1.Machine
+			fakeClient client.Client
 		)
 
 		BeforeEach(func() {
 			ctx = context.Background()
-			machine = common.NewMachine(defaultMachineName, defaultNamespace, defaultClusterName)
-			machine.Spec.Bootstrap = clusterv1.Bootstrap{
-				DataSecretName: &fakeBootstrapSecret,
-			}
-			Expect(k8sClient.Create(ctx, machine)).Should(Succeed())
-			ph, err := patch.NewHelper(capiCluster, k8sClient)
-			Expect(err).ShouldNot(HaveOccurred())
-			capiCluster.Status.InfrastructureReady = true
-			Expect(ph.Patch(ctx, capiCluster, patch.WithStatusObservedGeneration{})).Should(Succeed())
-
-			byoMachine = common.NewByoMachine(defaultByoMachineName, defaultNamespace, defaultClusterName, machine)
-			Expect(k8sClient.Create(ctx, byoMachine)).Should(Succeed())
 			byoHost = common.NewByoHost(hostname, defaultNamespace, nil)
 			Expect(k8sClient.Create(ctx, byoHost)).Should(Succeed())
+			fakeClient = fake.NewClientBuilder().WithObjects(
+				capiCluster,
+			).Build()
 
 		})
 
-		It("Should return error", func() {
-			byoMachineLookupkey := types.NamespacedName{Name: byoMachine.Name, Namespace: defaultNamespace}
-			request := reconcile.Request{NamespacedName: byoMachineLookupkey}
-			_, err := reconciler.Reconcile(ctx, request)
+		It("Should return node not found error", func() {
+			err := byoReconciler.setNodeProviderID(ctx, fakeClient, byoHost, "")
 			Expect(err).To(MatchError("nodes \"" + byoHost.Name + "\" not found"))
 		})
 
 		AfterEach(func() {
-			Expect(k8sClient.Delete(ctx, byoMachine)).Should(Succeed())
 			Expect(k8sClient.Delete(ctx, byoHost)).Should(Succeed())
-			Expect(k8sClient.Delete(ctx, machine)).Should(Succeed())
 		})
 	})
 
@@ -169,16 +159,11 @@ var _ = Describe("Controllers/ByomachineController/Unitests", func() {
 			})
 			Expect(err).NotTo(HaveOccurred(), "failed creating machineScope")
 
-			reconciler = &ByoMachineReconciler{
-				Client:  k8sClient,
-				Scheme:  &runtime.Scheme{},
-				Tracker: &remote.ClusterCacheTracker{},
-			}
 		})
 
 		It("should add cleanup annotation on byohost", func() {
 
-			Expect(reconciler.markHostForCleanup(ctx, machineScope)).NotTo(HaveOccurred(), "markHostForCleanup failed")
+			Expect(byoReconciler.markHostForCleanup(ctx, machineScope)).NotTo(HaveOccurred(), "markHostForCleanup failed")
 
 			createdByoHost := &infrastructurev1alpha4.ByoHost{}
 			Expect(k8sClient.Get(ctx, byoHostLookupKey, createdByoHost)).NotTo(HaveOccurred())
@@ -189,7 +174,7 @@ var _ = Describe("Controllers/ByomachineController/Unitests", func() {
 		})
 
 		It("should remove host reservation", func() {
-			Expect(reconciler.removeHostReservation(ctx, machineScope)).NotTo(HaveOccurred(), "host reservation removal failed")
+			Expect(byoReconciler.removeHostReservation(ctx, machineScope)).NotTo(HaveOccurred(), "host reservation removal failed")
 
 			createdByoHost := &infrastructurev1alpha4.ByoHost{}
 			Expect(k8sClient.Get(ctx, byoHostLookupKey, createdByoHost)).NotTo(HaveOccurred())
