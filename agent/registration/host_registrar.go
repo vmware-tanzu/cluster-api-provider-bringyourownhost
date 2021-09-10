a550e158790dde27e2abf5d41bd6bdd97f571cf3
huchen2021 <85480625+huchen2021@users.noreply.github.com>
2021-09-10 09:49:07 +0800

    TKG-6885 Host agent should support restart (#96)
    * TKG-6885 Host agent should support restart
    
    Currently when the agent start it will try to register it self to management cluster. we should add something there so that when the agent restart later it should avoid register itself again.
    
    Signed-off-by: Chen Hui <huchen@vmware.com>

--- agent/registration/host_registrar.go
+++ agent/registration/host_registrar.go
@@ -6,7 +6,10 @@ import (
 
 	"github.com/jackpal/gateway"
 	infrastructurev1alpha4 "github.com/vmware-tanzu/cluster-api-provider-byoh/apis/infrastructure/v1alpha4"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
 	"sigs.k8s.io/cluster-api/util/patch"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -18,28 +21,36 @@ type HostRegistrar struct {
 
 func (hr HostRegistrar) Register(hostName, namespace string) error {
 	ctx := context.TODO()
-	byoHost := &infrastructurev1alpha4.ByoHost{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ByoHost",
-			APIVersion: "infrastructure.cluster.x-k8s.io/v1alpha4",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      hostName,
-			Namespace: namespace,
-			Labels: map[string]string{
-				clusterv1.WatchLabel: hostName,
-			},
-		},
-		Spec:   infrastructurev1alpha4.ByoHostSpec{},
-		Status: infrastructurev1alpha4.ByoHostStatus{},
-	}
-
-	err := hr.K8sClient.Create(ctx, byoHost)
+	byoHost := &infrastructurev1alpha4.ByoHost{}
+	err := hr.K8sClient.Get(ctx, types.NamespacedName{Name: hostName, Namespace: namespace}, byoHost)
 	if err != nil {
-		return err
+		if !apierrors.IsNotFound(err) {
+			klog.Errorf("error getting host %s in namespace %s, err=%v", hostName, namespace, err)
+			return err
+		}
+		byoHost = &infrastructurev1alpha4.ByoHost{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ByoHost",
+				APIVersion: "infrastructure.cluster.x-k8s.io/v1alpha4",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      hostName,
+				Namespace: namespace,
+				Labels: map[string]string{
+					clusterv1.WatchLabel: hostName,
+				},
+			},
+			Spec:   infrastructurev1alpha4.ByoHostSpec{},
+			Status: infrastructurev1alpha4.ByoHostStatus{},
+		}
+		err = hr.K8sClient.Create(ctx, byoHost)
+		if err != nil {
+			klog.Errorf("error creating host %s in namespace %s, err=%v", hostName, namespace, err)
+			return err
+		}
 	}
 
-	// run it at startup
+	// run it at startup or reboot
 	return hr.UpdateNetwork(ctx, byoHost)
 }
 
