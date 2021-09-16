dc651094eb1407721196b6758acb80c722bafb63
Yuriy Georgiev (jeux) <u.georgiev@gmail.com>
2021-09-16 16:59:49 +0300

    Updated command line arguments check

--- hai/bundles/main.go
+++ hai/bundles/main.go
@@ -8,26 +8,28 @@ import (
 
 func main() {
 
-	if len(os.Args) > 1 {
-		builder := new(installer.Builder)
-		bundle := builder.NewInstaller()
-
-		if bundle == nil {
-			println("Unsupported OS/TKG pair")
-			os.Exit(0)
-		}
-
-		switch os.Args[1] {
-		case "install":
-			println("\n- INSTALL -")
-			bundle.Install()
-		case "uninstall":
-			println("\n- UNINSTALL -")
-			bundle.Uninstall()
-		default:
+	if len(os.Args) < 1 {
+		if os.Args[1] != "install" && os.Args[1] != "uninstall" {
 			println("Please specify operation: install, uninstall")
 		}
-	} else {
-		println("Please specify operation: install, uninstall")
+
+		os.Exit(-1)
+	}
+
+	builder := new(installer.Builder)
+	bundle := builder.NewInstaller()
+
+	if bundle == nil {
+		println("Unsupported OS/TKG pair")
+		os.Exit(-1)
+	}
+
+	switch os.Args[1] {
+	case "install":
+		println("\n- INSTALL -")
+		bundle.Install()
+	case "uninstall":
+		println("\n- UNINSTALL -")
+		bundle.Uninstall()
 	}
 }
