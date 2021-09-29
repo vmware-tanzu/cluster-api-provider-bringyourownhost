d432a7d96391405d9a7943bd99a02116347801f0
Anusha Hegde <anushah@vmware.com>
2021-09-29 12:51:29 +0530

    Remove test for asserting sentinel file cleanup

--- agent/reconciler/reconciler_test.go
+++ agent/reconciler/reconciler_test.go
@@ -237,15 +237,6 @@ runCmd:
 				byoHost.Annotations = map[string]string{hostCleanupAnnotation: ""}
 				conditions.MarkTrue(byoHost, infrastructurev1alpha4.K8sNodeBootstrapSucceeded)
 				Expect(patchHelper.Patch(ctx, byoHost, patch.WithStatusObservedGeneration{})).NotTo(HaveOccurred())
-
-				dir := filepath.Dir(bootstrapSentinelFile)
-				err := os.MkdirAll(dir, 0644)
-				Expect(err).NotTo(HaveOccurred())
-
-				f, err := os.Create(bootstrapSentinelFile)
-				Expect(err).NotTo(HaveOccurred())
-				defer f.Close()
-
 			})
 
 			It("should reset the node and set the Reason to K8sNodeAbsentReason", func() {
@@ -257,9 +248,6 @@ runCmd:
 
 				Expect(fakeCommandRunner.RunCmdCallCount()).To(Equal(1))
 				Expect(fakeCommandRunner.RunCmdArgsForCall(0)).To(Equal(KubeadmResetCommand))
-
-				_, err := os.Stat(bootstrapSentinelFile)
-				Expect(os.IsNotExist(err)).To(BeTrue())
 
 				updatedByoHost := &infrastructurev1alpha4.ByoHost{}
 				err = k8sClient.Get(ctx, byoHostLookupKey, updatedByoHost)
