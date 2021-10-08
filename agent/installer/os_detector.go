a1ef0c4b4c282b203f75ca2c7d7171cb7e524cab
georgievaVMW <90902341+georgievaVMW@users.noreply.github.com>
2021-10-08 16:55:19 +0300

    - Detect OS (#128)
    * Created a new file for the class oSDetector and test for its methods.
    
    * Started using ginkgo for testing.
    Changed filterSystemInfo to use regex.
    Changed the location and names of the files and package.
    Added more tests.
    
    * Made smaller changes and renamed the test suite.
    
    * Made changes to Makefile
    
    * Removed the git workflow for test-installer as it is already being tested by test-agent
    Added more tests for bad output from hostnamectl
    Made other smaller changes according to the reviews
    
    * Added test for detect() that it should not error with real hostnamectl
    
    * Removed spaces from Makefile and did some changes to method names.
    
    * delegateByHostnamectl ->DetectByHostnamectl
    removed mockGetHostnamectlCustomString
    
    * Made MockGetHostnamectl into a struct with counter

---
+++ agent/installer/os_detector.go
@@ -0,0 +1,113 @@
+package installer
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+const (
+	osNotDetected = "could not detect OS correctly"
+)
+
+// oSDetector contains all the logic for detecting the OS version.
+type osDetector struct {
+	cachedNormalizedOS string
+}
+
+// detect returns the os info in normalized format.
+// The format is as follows: <os>_<ver>_<arch>
+// Example with Ubuntu 21.04.3 64bit: Ubuntu_20.04.3_x64
+func (osd *osDetector) Detect() (string, error) {
+	return osd.DetectByHostnamectl(func() (string, error) { return osd.getHostnamectl() })
+}
+
+// DetectByHostnamectl is a helper method to enable testing of detect with mock methods.
+func (osd *osDetector) DetectByHostnamectl(f func() (string, error)) (string, error) {
+	if osd.cachedNormalizedOS != "" {
+		return osd.cachedNormalizedOS, nil
+	}
+
+	systemInfo, err := f()
+	if err != nil {
+		return "", err
+	}
+	osDetails := parseHostnamectl(systemInfo)
+	os := osDetails[0]
+	ver := osDetails[1]
+	arch := osDetails[2]
+	if os == "" || ver == "" || arch == "" {
+		return "", errors.New(osNotDetected)
+	}
+
+	osd.cachedNormalizedOS = normalizeOSName(os, ver, arch)
+	return osd.cachedNormalizedOS, nil
+}
+
+// normalizeOsName normalizes given os, arch and k8s version to the correct format.
+// Takes as arguments os, ver and arch then returns string in the format <os>_<ver>_<arch>
+func normalizeOSName(os, ver, arch string) string {
+	osName := fmt.Sprintf("%s_%s_%s", os, ver, arch)
+	osName = strings.ReplaceAll(osName, " ", "_")
+
+	return osName
+}
+
+// getHostSystemInfo returns the result after executing a preset command.
+// Exact output format varies between different distributions but the important
+// part is the line starting with the string  "Operating system:"  which  shows
+// the exact version of the operating  system.  This  information  is  used  to
+// identify the correct installer that needs to be used. Also used is the  line
+// starting with "Architecture: " to identify whether we need the 32 or 64  bit
+// bundle.
+//
+// Example output for running the command on Ubuntu:
+//
+//  Static hostname: ubuntu
+//        Icon name: computer-vm
+//          Chassis: vm
+//       Machine ID: 242642b0e734472abaf8c5337e1174c4
+//          Boot ID: 181f08d651b76h39be5b138231427c5c
+//   Virtualization: vmware
+// Operating System: Ubuntu 20.04.3 LTS
+//           Kernel: Linux 5.11.0-27-generic
+//     Architecture: x86-64
+func (osd *osDetector) getHostnamectl() (string, error) {
+	out, err := exec.Command("hostnamectl").Output()
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+// Method that extracts the important information from getHostSystemInfo.
+func parseHostnamectl(systemInfo string) [3]string {
+	const strIndicatingOSline string = "Operating System: "
+	const strIndicatingArchline string = "Architecture: "
+
+	var os, ver, arch string
+
+	osRegex := regexp.MustCompile(strIndicatingOSline + `[a-zA-Z]+[ a-zA-Z]*[a-zA-Z]+`)
+	locOS := osRegex.FindIndex([]byte(systemInfo))
+	if locOS != nil {
+		os = systemInfo[locOS[0]+len(strIndicatingOSline) : locOS[1]]
+	}
+
+	verRegex := regexp.MustCompile(strIndicatingOSline + `[a-zA-Z]+[ a-zA-Z]* (\d+(\.\d+)*)`)
+	locVer := verRegex.FindIndex([]byte(systemInfo))
+	if locVer != nil {
+		ver = systemInfo[locOS[1]+1 : locVer[1]]
+	}
+
+	archRegex := regexp.MustCompile(strIndicatingArchline + `[a-zA-Z]+[ a-zA-Z0-9-]*`)
+	locArch := archRegex.FindIndex([]byte(systemInfo))
+	if locArch != nil {
+		arch = systemInfo[locArch[0]+len(strIndicatingArchline) : locArch[1]]
+	}
+
+	return [3]string{os, ver, arch}
+}
---
+++ agent/installer/os_detector_test.go
@@ -0,0 +1,109 @@
+package installer
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type MockHostnamectl struct {
+	callCount int
+}
+
+func (mh *MockHostnamectl) Get(os, ver, arch string) (string, error) {
+	mh.callCount++
+	out := "  Static hostname: ubuntu\n" +
+		"        Icon name: computer-vm\n" +
+		"          Chassis: vm\n" +
+		"       Machine ID: 242642b0e734472abaf8c5337e1174c4\n" +
+		"          Boot ID: 181f08d651b76h39be5b138231427c5c\n" +
+		"   Virtualization: vmware\n" +
+		" Operating System: " + os + " " + ver + " LTS\n" +
+		"           Kernel: Linux 5.11.0-27-generic\n" +
+		"     Architecture: " + arch + "\n"
+
+	return out, nil
+}
+
+var _ = Describe("Byohost Installer Tests", func() {
+
+	var (
+		d          *osDetector
+		mh         *MockHostnamectl
+		os         string
+		ver        string
+		arch       string
+		detectedOS string
+		err        error
+	)
+
+	BeforeEach(func() {
+		d = &osDetector{}
+		mh = &MockHostnamectl{}
+		os = "Ubuntu"
+		ver = "20.04.3"
+		arch = "x86-64"
+	})
+
+	Context("When the OS is detected", func() {
+		It("Should return string in normalized format", func() {
+			detectedOS, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).ShouldNot((HaveOccurred()))
+			Expect(detectedOS).To(Equal("Ubuntu_20.04.3_x86-64"))
+		})
+		It("Should cache OS and not execute again getHostnamectl", func() {
+			_, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).ShouldNot((HaveOccurred()))
+			Expect(mh.callCount).To(Equal(1))
+			_, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).ShouldNot((HaveOccurred()))
+			Expect(mh.callCount).To(Equal(1))
+		})
+
+		It("Should return string in normalized format and work with OS names with more than one word", func() {
+			os = "Red Hat Enterprise Linux"
+			ver = "8.1"
+			detectedOS, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).ShouldNot((HaveOccurred()))
+			Expect(detectedOS).To(Equal("Red_Hat_Enterprise_Linux_8.1_x86-64"))
+		})
+
+		It("Should not error with real hostnamectl", func() {
+			_, err = d.Detect()
+			Expect(err).ShouldNot((HaveOccurred()))
+		})
+	})
+	Context("When the OS is not detected", func() {
+		It("Should return error if OS distribution is missing", func() {
+			os = ""
+			_, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).Should((HaveOccurred()))
+		})
+
+		It("Should return error if OS version is missing", func() {
+			ver = ""
+			_, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).Should((HaveOccurred()))
+		})
+
+		It("Should return error if OS architecture is missing", func() {
+			arch = ""
+			_, err = d.DetectByHostnamectl(func() (string, error) { return mh.Get(os, ver, arch) })
+			Expect(err).Should((HaveOccurred()))
+		})
+
+		It("Should return error if output is missing", func() {
+			_, err = d.DetectByHostnamectl(func() (string, error) {
+				return "", nil
+			})
+			Expect(err).Should((HaveOccurred()))
+		})
+
+		It("Should return error if output is random string", func() {
+			_, err = d.DetectByHostnamectl(func() (string, error) {
+				return "wef9sdf092g\nd2g39\n\n\nd92faad", nil
+			})
+			Expect(err).Should((HaveOccurred()))
+		})
+	})
+
+})
