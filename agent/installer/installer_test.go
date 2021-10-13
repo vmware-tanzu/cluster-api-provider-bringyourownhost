5eb02e731e7a861f76949432efd14b4d72b2f4ca
georgieva <georgieva@vmware.com>
2021-10-13 16:59:45 +0300

    Checking if fixing an error that came up in other file fixes the tests.

--- agent/installer/installer_test.go
+++ agent/installer/installer_test.go
@@ -1,16 +1,17 @@
 package installer
 
 import (
+	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Byohost Installer Tests", func() {
-	 Context("When installer is created", func() {
-                It("Should return error", func() {
-			_, err := New("repo", "downloadPath", nil)
+	Context("When installer is created", func() {
+		It("Should return error", func() {
+			_, err := New("repo", "downloadPath", logr.Logger{})
 			Expect(err).ShouldNot((HaveOccurred()))
-                })
+		})
 	})
 
 })
