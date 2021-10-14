7beaabe04212f6ec69007ef13b1f2660b4d0edf3
georgieva <georgieva@vmware.com>
2021-10-14 15:40:34 +0300

    No changes to the bundle_downloader files. Downgraded imgpkg to version 0.18 because 0.20 is using go 1.17 and newer version of logr that breaks the project.

--- agent/installer/installer_test.go
+++ agent/installer/installer_test.go
@@ -1,17 +1,16 @@
 package installer
 
 import (
-	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Byohost Installer Tests", func() {
-	Context("When installer is created", func() {
-		It("Should return error", func() {
-			_, err := New("repo", "downloadPath", logr.Logger{})
+	 Context("When installer is created", func() {
+                It("Should return error", func() {
+			_, err := New("repo", "downloadPath", nil)
 			Expect(err).ShouldNot((HaveOccurred()))
-		})
+                })
 	})
 
 })
