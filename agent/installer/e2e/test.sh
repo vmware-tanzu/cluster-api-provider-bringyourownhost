c3983f113bb8ce3dd7ae0a7db6b619cb0045e55b
Rousko Atanasov <ratanasov@vmware.com>
2021-11-18 15:38:39 +0200

    Run from local dir

--- agent/installer/e2e/test.sh
+++ agent/installer/e2e/test.sh
@@ -8,9 +8,10 @@ export K8SVER="v1.22.3"
 
 function cleanup()
 {
+    ARG=$?
+
     set +e
 
-    ARG=$?
     echo "==Clean up"
 
     docker-compose -p $TAG -f build.yml -f core.yml down --rmi all --volumes
@@ -20,14 +21,10 @@ function cleanup()
     rm cli
     rm -rf $K8SVER*
 
-    popd
-
     exit $ARG
 }
 
 trap cleanup EXIT
-
-pushd agent/installer/e2e
 
 echo "===Download bundle ingredients"
 docker-compose -p $TAG -f core.yml -f build.yml up --build ingredients-deb
@@ -44,3 +41,5 @@ echo "===Spin up test vm"
 vagrant up
 echo "===Install bundle inside vm"
 vagrant ssh -c "cd /vagrant && sudo ./test-in-vm.sh $REPOPORT $K8SVER"
+
+echo "===PASS"
