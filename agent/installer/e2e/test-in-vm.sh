2723c25dbfcf1424142a9b303ed1cece1c508e31
Rousko Atanasov <ratanasov@vmware.com>
2021-11-18 14:57:09 +0200

    rename files

---
+++ agent/installer/e2e/test-in-vm.sh
@@ -0,0 +1,21 @@
+#~/bin/bash
+
+# host machine is accessible as gateway by guest
+REPO_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+REPO_PORT=$1
+K8SVER=$2
+
+echo "===Install Pre-requisites"
+apt-get install socat ebtables ethtool conntrack -y
+
+set -e
+
+echo "===Run Install"
+./cli --install --bundle-repo $REPO_IP:$REPO_PORT/byoh --k8s $K8SVER
+
+echo "===Run kubeadm preflight"
+kubeadm init phase preflight --ignore-preflight-errors=Cpu,Mem
+
+echo "===Run Uninstall"
+./cli --uninstall --k8s $K8SVER
+
--- agent/installer/e2e/test.sh
+++ agent/installer/e2e/test.sh
@@ -1,21 +1,46 @@
 #~/bin/bash
 
-# host machine is accessible as gateway by guest
-REPO_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-REPO_PORT=$1
-K8SVER=$2
-
-echo "===Install Pre-requisites"
-apt-get install socat ebtables ethtool conntrack -y
-
 set -e
 
-echo "===Run Install"
-./cli --install --bundle-repo $REPO_IP:$REPO_PORT --k8s $K8SVER
+TAG=byoh-installer-e2e-test
+export REPOPORT=5005
+export K8SVER="v1.22.3"
 
-echo "===Run kubeadm preflight"
-kubeadm init phase preflight --ignore-preflight-errors=Cpu,Mem
+function cleanup()
+{
+    set +e
 
-echo "===Run Uninstall"
-./cli --uninstall --bundle-repo $REPO_IP:$REPO_PORT --k8s $K8SVER
+    ARG=$?
+    echo "==Clean up"
 
+    docker-compose -p $TAG -f build.yml -f core.yml down --rmi all --volumes
+
+    vagrant destroy -f
+
+    rm cli
+    rm -rf $K8SVER*
+
+    popd
+
+    exit $ARG
+}
+
+trap cleanup EXIT
+
+pushd agent/installer/e2e
+
+echo "===Download bundle ingredients"
+docker-compose -p $TAG -f core.yml -f build.yml up --build ingredients-deb
+
+echo "===Starting bundle repository"
+docker-compose -p $TAG -f core.yml -f build.yml up --build bundle-repo.local &
+
+echo "===Build and publish bundle"
+docker-compose -p $TAG -f core.yml -f build.yml up --build bundle-builder
+
+echo "===Build cli"
+go build ../cli
+echo "===Spin up test vm"
+vagrant up
+echo "===Install bundle inside vm"
+vagrant ssh -c "cd /vagrant && sudo ./test-in-vm.sh $REPOPORT $K8SVER"
