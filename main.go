996b941cbb38200c2dd31c4041a677bcb5eec4a8
Sachin Singh <sachins2@vmware.com>
2022-03-15 13:36:24 +0000

    Refactored `./main.go` to avoid linting errors
    The linter was raising issues for number of lines in main so refactored
    the initialization of flags to a separeate function.
    Also added the enviornment variable for experimental secure access flag
    for the controller in the management cluster.
    Removed the experimental `abc.txt`.

--- main.go
+++ main.go
@@ -33,8 +33,11 @@ import (
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme               = runtime.NewScheme()
+	setupLog             = ctrl.Log.WithName("setup")
+	metricsAddr          string
+	enableLeaderElection bool
+	probeAddr            string
 )
 
 func init() {
@@ -48,15 +51,18 @@ func init() {
 	utilruntime.Must(admissionv1beta1.AddToScheme(scheme))
 }
 
-func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
+func setupflags() {
+	klog.InitFlags(nil)
+
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	feature.MutableGates.AddFlag(pflag.CommandLine)
+}
+
+func main() {
+	setupflags()
 	pflag.Parse()
 
 	ctrl.SetLogger(klogr.New())
