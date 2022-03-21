58e5a3913aefe29bd2542ec0d06d287cab8d8fba
Yuriy Georgiev (jeux) <u.georgiev@gmail.com>
2022-03-21 10:21:47 +0200

    Implemented K8s patch version masking in the registry matrix of the Installer (#430)
    * Implemented K8s patch version masking via registry filter similar to the OS one
    
    * Revert "Implemented K8s patch version masking via registry filter similar to the OS one"
    
    This reverts commit fdbfbb293cd76eb16889fefff416e7c9c1a459c8.
    
    * Implemented K8s patch version masking via registry filter similar to the OS one
    
    * Updated the registry e2e test
    
    * Resolved hardcoded k8s version requirement from the Installer
    
    * Resolved issue causing the installer to panic when no k8s version is specified when calling the addBundleInstaller - it now defaults to v1.22.3 if no k8s ver is specified
    
    * Default K8s version set to v1.22 if no such is provided; Any patch subversion is accepted if supported Major and Minor versions are present.
    
    * Added filters for K8s versions v1.21.*, v1.22.*, v1.23.*
    
    * Revert "Added filters for K8s versions v1.21.*, v1.22.*, v1.23.*"
    
    This reverts commit eb3a0338c991553686b5cfc4b29b9ed178728868.
    
    * Revert "Added filters for K8s versions v1.21.*, v1.22.*, v1.23.*"
    
    This reverts commit eb3a0338c991553686b5cfc4b29b9ed178728868.
    
    There is a separate issue for this task.
    
    * Updated K8s against algo mapping to require specific hardcoded mask versions
    
    * Added empty lines between couple of comments
    
    * Refactored the filter container from plain array to struct for consistency reasons; Updated the registry e2e tests
    
    * Removed unused registry test variable
    
    * Renamed the supported OS container structure to better represent it purpose.

--- agent/installer/registry.go
+++ agent/installer/registry.go
@@ -11,18 +11,26 @@ import (
 type osk8sInstaller interface{}
 type k8sInstallerMap map[string]osk8sInstaller
 type osk8sInstallerMap map[string]k8sInstallerMap
-type filterBundlePair struct {
+type filterOsBundlePair struct {
 	osFilter string
 	osBundle string
 }
-type filterBundleList []filterBundlePair
+
+type filterK8sBundle struct {
+	k8sFilter string
+}
+
+type filterOSBundleList []filterOsBundlePair
+type filterK8sBundleList []filterK8sBundle
 
 // Registry contains
 // 1. Entries associating BYOH Bundle i.e. (OS,K8sVersion) in the Repository with Installer in Host Agent
 // 2. Entries that match a concrete OS to a BYOH Bundle OS from the Repository
+// 3. Entries that match a Major & Minor versions of K8s to any of their patch sub-versions (e.g.: 1.22.3 -> 1.22.*)
 type registry struct {
 	osk8sInstallerMap
-	filterBundleList
+	filterOSBundleList
+	filterK8sBundleList
 }
 
 func newRegistry() registry {
@@ -42,14 +50,18 @@ func (r *registry) AddBundleInstaller(os, k8sVer string, installer osk8sInstalle
 }
 
 func (r *registry) AddOsFilter(osFilter, osBundle string) {
-	r.filterBundleList = append(r.filterBundleList, filterBundlePair{osFilter: osFilter, osBundle: osBundle})
+	r.filterOSBundleList = append(r.filterOSBundleList, filterOsBundlePair{osFilter: osFilter, osBundle: osBundle})
+}
+
+func (r *registry) AddK8sFilter(k8sFilter string) {
+	r.filterK8sBundleList = append(r.filterK8sBundleList, filterK8sBundle{k8sFilter: k8sFilter})
 }
 
 func (r *registry) ListOS() (osFilter, osBundle []string) {
-	osFilter = make([]string, 0, len(r.filterBundleList))
-	osBundle = make([]string, 0, len(r.filterBundleList))
+	osFilter = make([]string, 0, len(r.filterOSBundleList))
+	osBundle = make([]string, 0, len(r.filterOSBundleList))
 
-	for _, fbp := range r.filterBundleList {
+	for _, fbp := range r.filterOSBundleList {
 		osFilter = append(osFilter, fbp.osFilter)
 		osBundle = append(osBundle, fbp.osBundle)
 	}
@@ -79,15 +91,27 @@ func (r *registry) ListK8s(osBundleHost string) []string {
 
 func (r *registry) GetInstaller(osHost, k8sVer string) (osk8si osk8sInstaller, osBundle string) {
 	osBundle = r.resolveOsToOsBundle(osHost)
-	osk8si = r.osk8sInstallerMap[osBundle][k8sVer]
+	k8sBundle := r.resolveK8sToK8sBundle(k8sVer)
+	osk8si = r.osk8sInstallerMap[osBundle][k8sBundle]
 	return
 }
 
 func (r *registry) resolveOsToOsBundle(os string) string {
-	for _, fbp := range r.filterBundleList {
+	for _, fbp := range r.filterOSBundleList {
 		matched, _ := regexp.MatchString(fbp.osFilter, os)
 		if matched {
 			return fbp.osBundle
+		}
+	}
+
+	return ""
+}
+
+func (r *registry) resolveK8sToK8sBundle(k8s string) string {
+	for _, k8sBundle := range r.filterK8sBundleList {
+		matched, _ := regexp.MatchString(k8sBundle.k8sFilter, k8s)
+		if matched {
+			return k8sBundle.k8sFilter
 		}
 	}
 
--- agent/installer/registry_test.go
+++ agent/installer/registry_test.go
@@ -14,10 +14,9 @@ var _ = Describe("Byohost Installer Tests", func() {
 		type dummyinstaller int
 
 		const (
-			dummy122  = dummyinstaller(122)
+			dummy122  = dummyinstaller(1221)
 			dummy1122 = dummyinstaller(1122)
-			dummy123  = dummyinstaller(123)
-			dummy124  = dummyinstaller(124)
+			dummy124  = dummyinstaller(1243)
 		)
 
 		var (
@@ -36,23 +35,16 @@ var _ = Describe("Byohost Installer Tests", func() {
 			Expect(r.GetInstaller("a", "b")).To(BeNil())
 		})
 		It("Should allow working with installers", func() {
-			Expect(func() { r.AddBundleInstaller("ubuntu", "1.22", dummy122) }).NotTo(Panic())
-			Expect(func() { r.AddBundleInstaller("ubuntu", "1.23", dummy123) }).NotTo(Panic())
-			Expect(func() { r.AddBundleInstaller("rhel", "1.24", dummy124) }).NotTo(Panic())
+			Expect(func() { r.AddBundleInstaller("ubuntu", "v1.22.*", dummy122) }).NotTo(Panic())
+			Expect(func() { r.AddBundleInstaller("rhel", "v1.22.*", dummy124) }).NotTo(Panic())
+
 			r.AddOsFilter("ubuntu.*", "ubuntu")
 			r.AddOsFilter("rhel.*", "rhel")
+			r.AddK8sFilter("v1.22.*")
 
-			inst, osBundle := r.GetInstaller("ubuntu-1", "1.22")
+			inst, osBundle := r.GetInstaller("ubuntu-1", "v1.22.1")
 			Expect(inst).To(Equal(dummy122))
 			Expect(osBundle).To(Equal("ubuntu"))
-
-			inst, osBundle = r.GetInstaller("ubuntu-2", "1.23")
-			Expect(inst).To(Equal(dummy123))
-			Expect(osBundle).To(Equal("ubuntu"))
-
-			inst, osBundle = r.GetInstaller("rhel-1", "1.24")
-			Expect(inst).To(Equal(dummy124))
-			Expect(osBundle).To(Equal("rhel"))
 
 			osFilters, osBundles := r.ListOS()
 			Expect(osFilters).To(ContainElements("rhel.*", "ubuntu.*"))
@@ -60,13 +52,13 @@ var _ = Describe("Byohost Installer Tests", func() {
 			Expect(osBundles).To(ContainElements("rhel", "ubuntu"))
 			Expect(osBundles).To(HaveLen(2))
 
-			Expect(r.ListK8s("ubuntu")).To(ContainElements("1.22", "1.23"))
-			Expect(r.ListK8s("ubuntu")).To(HaveLen(2))
+			Expect(r.ListK8s("ubuntu")).To(ContainElements("v1.22.*"))
+			Expect(r.ListK8s("ubuntu")).To(HaveLen(1))
 
-			Expect(r.ListK8s("rhel")).To(ContainElement("1.24"))
+			Expect(r.ListK8s("rhel")).To(ContainElement("v1.22.*"))
 			Expect(r.ListK8s("rhel")).To(HaveLen(1))
 
-			Expect(r.GetInstaller("photon", "1.22")).To(BeNil())
+			Expect(r.GetInstaller("photon", "v1.22.1")).To(BeNil())
 			osFilters, osBundles = r.ListOS()
 			Expect(osFilters).To(ContainElements("rhel.*", "ubuntu.*"))
 			Expect(osFilters).To(HaveLen(2))
@@ -76,10 +68,11 @@ var _ = Describe("Byohost Installer Tests", func() {
 		})
 		It("Should decouple host os from bundle os", func() {
 			// Bundle OS does not match filter OS
-			r.AddBundleInstaller("UBUNTU", "1.22", dummy122)
+			r.AddBundleInstaller("UBUNTU", "v1.22.*", dummy122)
 			r.AddOsFilter("ubuntu.*", "UBUNTU")
+			r.AddK8sFilter("v1.22.*")
 
-			inst, osBundle := r.GetInstaller("ubuntu-1", "1.22")
+			inst, osBundle := r.GetInstaller("ubuntu-1", "v1.22.1")
 			Expect(inst).To(Equal(dummy122))
 			Expect(osBundle).To(Equal("UBUNTU"))
 
@@ -90,11 +83,11 @@ var _ = Describe("Byohost Installer Tests", func() {
 
 			// ListK8s should work with both
 			osBundleResult := r.ListK8s("UBUNTU")
-			Expect(osBundleResult).To(ContainElements("1.22"))
+			Expect(osBundleResult).To(ContainElements("v1.22.*"))
 			Expect(osBundleResult).To(HaveLen(1))
 
 			osHostResult := r.ListK8s("ubuntu-20-04")
-			Expect(osHostResult).To(ContainElements("1.22"))
+			Expect(osHostResult).To(ContainElements("v1.22.*"))
 			Expect(osHostResult).To(HaveLen(1))
 		})
 		It("Should panic on duplicate installers", func() {
@@ -103,8 +96,24 @@ var _ = Describe("Byohost Installer Tests", func() {
 			 * Adding a mapping to already existing os and k8s is clearly a typo and bug.
 			 * Make it obvious
 			 */
-			Expect(func() { r.AddBundleInstaller("ubuntu", "1.22", dummy122) }).NotTo(Panic())
-			Expect(func() { r.AddBundleInstaller("ubuntu", "1.22", dummy1122) }).To(Panic())
+			Expect(func() { r.AddBundleInstaller("ubuntu", "v1.22.*", dummy122) }).NotTo(Panic())
+			Expect(func() { r.AddBundleInstaller("ubuntu", "v1.22.*", dummy1122) }).To(Panic())
+		})
+		It("Should not find unsupported K8s versions", func() {
+			Expect(func() { r.AddBundleInstaller("ubuntu", "v1.22.*", dummy122) }).NotTo(Panic())
+			Expect(func() { r.AddBundleInstaller("rhel", "v1.22.*", dummy122) }).NotTo(Panic())
+
+			// Intentionally skip adding the following filters for unsuported K8s:
+			// AddK8sFilter("v1.93.*")
+			// AddK8sFilter("v1.94.*")
+
+			inst, osBundle := r.GetInstaller("ubuntu", "v1.93.2")
+			Expect(inst).To(BeNil())
+			Expect(osBundle).To(Equal(""))
+
+			inst, osBundle = r.GetInstaller("rhel", "v1.94.3")
+			Expect(inst).To(BeNil())
+			Expect(osBundle).To(Equal(""))
 		})
 	})
 })
