a77bf5d3754f6881239b85685981b5c7f80f5edd
Sachin Kumar Singh <sachins2@vmware.com>
2022-04-08 14:27:39 +0530

    Platform specific fields in BYOHost CRD
    Signed-off-by: Sachin Kumar Singh <sachins2@vmware.com>

--- apis/infrastructure/v1beta1/byohost_types.go
+++ apis/infrastructure/v1beta1/byohost_types.go
@@ -32,6 +32,26 @@ type ByoHostSpec struct {
 	BootstrapSecret *corev1.ObjectReference `json:"bootstrapSecret,omitempty"`
 }
 
+// HostStatus is a set of details about the host platform.
+type HostStatus struct {
+	// MachineID reported by the host. For unique machine identification
+	// in the cluster this field is preferred. Learn more from man(5)
+	// machine-id: http://man7.org/linux/man-pages/man5/machine-id.5.html
+	HostID string `json:"hostid,omitempty"`
+
+	// The Operating System reported by the host.
+	OSName string `json:"osname,omitempty"`
+
+	// OS Image reported by the host.
+	OSImage string `json:"osimage,omitempty"`
+
+	// Kernel Version reported by the host.
+	KernelVersion string `json:"kernelversion,omitempty"`
+
+	// The Architecture reported by the host.
+	Architecture string `json:"architecture,omitempty"`
+}
+
 // ByoHostStatus defines the observed state of ByoHost
 type ByoHostStatus struct {
 	// MachineRef is an optional reference to a Cluster API Machine
@@ -42,6 +62,10 @@ type ByoHostStatus struct {
 	// Conditions defines current service state of the BYOMachine.
 	// +optional
 	Conditions clusterv1.Conditions `json:"conditions,omitempty"`
+
+	// HostInfo returns the platform details of the host.
+	// +optional
+	HostInfo HostStatus `json:"hostinfo,omitempty"`
 
 	// Network returns the network status for each of the host's configured
 	// network interfaces.
